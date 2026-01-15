c5a332da591204fe2d35a1c560a78209c6b1db87
HimanshuInRaoInfo <himanshu@raoinformationtechnology.com>
2026-01-15 19:05:10 +0530

    information store path change

--- src/setup_browser_json.js
+++ src/setup_browser_json.js
@@ -10,7 +10,7 @@ class SetupBrowserJSONData {
         const roamingPath = process.env.APPDATA;
         console.log("Browser information shows here", browserData);
         if (platform === 'Windows_NT' || platform === 'Linux') {
-            const filePath = path.join(roamingPath, folderName, 'browsersInformation.json');
+            const filePath = path.join(roamingPath, folderName, "browser_storage", 'browsersInformation.json');
             console.log("File path to store", filePath)
             fs.writeFileSync(filePath, JSON.stringify(browserData, null, 2), 'utf-8');
             console.log('browsersInformation.json created with this path', filePath);
@@ -22,7 +22,7 @@ class SetupBrowserJSONData {
     getFileData(folderName) {
         try {
             const roamingPath = process.env.APPDATA;
-            const filePath = path.join(roamingPath, folderName, 'browsersInformation.json');
+            const filePath = path.join(roamingPath, folderName, "browser_storage", 'browsersInformation.json');
             let result = fs.readFileSync(filePath, 'utf8');
             return JSON.parse(result);
         } catch (err) {
